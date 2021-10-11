be8076fa764a3f01955a908ca907b8a24a8fc6cc
Aleksandr <klenovaleks98@mail.ru>
2021-10-11 15:01:08 +0300

    Initial commit

--- events/admin.py
+++ events/admin.py
@@ -3,15 +3,79 @@ from . import models
 
 # Register your models here.
 
+class PlaceLeftFilter(admin.SimpleListFilter):
+    title = 'Заполненность события'
+    parameter_name = 'places_left_filter'
+
+    def lookups(self, request, model_admin):
+        filter_list = [
+            ('0', '<=50%'),
+            ('1', '>50%'),
+            ('2', 'sold-out')
+        ]
+        return filter_list
+
+
+    def queryset(self, request, queryset):
+        filter_value = self.value()
+        for obj in queryset:
+            new_obj = obj
+            new_obj.res=models.Event.result(obj)
+            new_obj.save()
+
+        if filter_value == '0':
+            return queryset.filter(res__gte=0, res__lte=0.5)
+        elif filter_value == '1':
+            return queryset.filter(res__gt=0.5,res__lt=1)
+        elif filter_value == '2':
+            return queryset.filter(res=1)
+        return queryset
+
+class ReviewInline(admin.TabularInline):
+    model = models.Review
+    readonly_fields = ('id','user','rate','text','created','updated',)
+    can_delete = False
+
+    def has_add_permission(self, request, obj):
+        if obj:
+            return False
+
 
 @admin.register(models.Event)
 class EventAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'title', 'date_start','participants_number','category','is_private','display_enroll_count', ]
+    list_display.append('display_places_left')
+    list_select_related = ['category',]
+    list_filter = [PlaceLeftFilter, 'category', 'features', ]
+    search_fields = ['title',]
+    readonly_fields = ['display_enroll_count','display_places_left']
+    exclude = ['res']
+    filter_horizontal = ['features',]
+    inlines = [ReviewInline]
+
+
 
 
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id','display_event_count', 'title', ]
+    list_display_links = ['id', 'title', ]
 
+
+@admin.register(models.Feature)
+class FeatureAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', ]
+    list_display_links = ['id', 'title', ]
+
+@admin.register(models.Enroll)
+class EnrollAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'event', ]
+    list_display_links = ['id', 'user', 'event', ]
+
+@admin.register(models.Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['created', 'user', 'event', 'updated', 'id', ]
+    list_display_links = ['id', 'user', 'event', ]
+    list_filter = ['created','event',]
 
 
--- events/models.py
+++ events/models.py
@@ -1,6 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Feature(models.Model):
+    title = models.CharField(max_length=90, blank=True, default='', verbose_name='Свойство события')
+
+    class Meta:
+        verbose_name = 'Свойство'
+        verbose_name_plural = 'Свойства'
+
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    title = models.CharField(max_length=90, blank=True, default='', verbose_name='Категория')
+
+    def display_event_count(self):
+        return len(self.events.all())
+    display_event_count.short_description = 'Количество событий'
+
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.title
 
 class Event(models.Model):
     title = models.CharField(max_length=200, blank=True, default='', verbose_name='Название')
@@ -8,18 +33,68 @@ class Event(models.Model):
     date_start = models.DateTimeField(verbose_name='Дата начала')
     participants_number = models.PositiveSmallIntegerField(verbose_name='Количество участников')
     is_private = models.BooleanField(default=False, verbose_name='Частное')
+    category = models.ForeignKey(Category,null=True,on_delete=models.CASCADE,related_name='events')
+    features = models.ManyToManyField(Feature)
+
+
+    def display_enroll_count(self):
+        return len(self.enrolls.all())
+
+    display_enroll_count.short_description = 'Количество записей'
+
+    res = models.FloatField(blank=True)
+
+    def result(self):
+        self.res = len(self.enrolls.all())/self.participants_number
+        return self.res
+
+    def display_places_left(self):
+        col = self.participants_number-len(self.enrolls.all())
+        if col>= self.participants_number/2:
+            return f'{col}(<=50%)'
+        elif col< self.participants_number/2 and col !=0:
+            return f'{col}(>50%)'
+        elif col == 0:
+            return f'{col}(sold-out)'
+
+    display_places_left.short_description = 'Осталось мест'
+
 
     class Meta:
         verbose_name = 'Событие'
         verbose_name_plural = 'События'
+        ordering = ['date_start']
+
+    def __str__(self):
+        return self.title
 
 
-class Category(models.Model):
-    title = models.CharField(max_length=90, blank=True, default='', verbose_name='Категория')
+class Enroll(models.Model):
+    user = models.ForeignKey(User, blank=True, on_delete = models.CASCADE,related_name='enrolls')
+    event = models.ForeignKey(Event, blank=True, on_delete = models.CASCADE,related_name='enrolls')
+    created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
+        verbose_name = 'Запись'
+        verbose_name_plural = 'Записи'
+
+    def __str__(self):
+        return self.event
+
+class Review(models.Model):
+    user = models.ForeignKey(User, blank=True, on_delete = models.CASCADE,related_name='reviews')
+    event = models.ForeignKey(Event, blank=True, on_delete = models.CASCADE,related_name='reviews')
+    created = models.DateTimeField(auto_now_add=True)
+    rate = models.PositiveSmallIntegerField()
+    updated = models.DateTimeField(auto_now=True)
+    text = models.TextField(blank=True, default='', verbose_name='текст отзыва')
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+
+
+
 
 
 
