1717c0f15c4d69ec6617a74b03348647b7f65bad
Aleksandr <klenovaleks98@mail.ru>
2021-11-13 14:17:29 +0300

    Homework 5

--- events/models.py
+++ events/models.py
@@ -46,8 +46,6 @@ class Event(models.Model):
         (FULLNESS_FULL, FULLNESS_LEGEND_FULL),
     )
 
-    objects = models.Manager()
-    objects_event_qs = EventQuerySet.as_manager()
     title = models.CharField(max_length=200, blank=True, default='', verbose_name='Название')
     description = models.TextField(blank=True, default='', verbose_name='Описание')
     date_start = models.DateTimeField(verbose_name='Дата начала')
