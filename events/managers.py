e23ad7843756f509f90046f4ece96e72fa3be9bd
Aleksandr <klenovaleks98@mail.ru>
2021-11-13 15:19:08 +0300

    Homework 5

--- events/managers.py
+++ events/managers.py
@@ -1,19 +1,20 @@
 from django.db import models
-from django.db.models import Q, Prefetch
 from django.db.models.functions import Coalesce
-
 
 
 class EventQuerySet(models.QuerySet):
     def with_counts(self):
         return self.annotate(
-            count=Coalesce(models.Count('enrolls'), 0),
-            #available=models.F('participants_number') - self.count,
-            #available=models.F('participants_number')-models.F('count'),
-            available=models.F('participants_number') - models.Count('enrolls')
-            #available = models.F('participants_number') - models.Max(display_enroll_count())
+            count=Coalesce(models.Count('enrolls', distinct=True), 0),
+            places_left=models.F('participants_number') - models.Count('enrolls', distinct=True),
+            get_rate=models.Avg('reviews__rate')
         )
 
-    def EvQuSet(self):
-        return self.select_related('category').prefetch_related('enrolls__user', 'features',
-                    'reviews__user').with_counts()
+    def event_qs(self):
+        return self.select_related(
+            'category',
+        ).prefetch_related(
+            'enrolls__user',
+            'features',
+            'reviews__user',
+        ).with_counts()
