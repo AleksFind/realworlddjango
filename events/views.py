7882da7a2d32693d706753279c64bf676984b290
Aleksandr <klenovaleks98@mail.ru>
2021-11-13 15:00:38 +0300

    Homework 5

--- events/views.py
+++ events/views.py
@@ -39,7 +39,7 @@ class EventListView(ListView):
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        queryset = queryset.event_qs()
+        queryset = queryset.event_qs(self)
         form = EventFilterForm(self.request.GET)
         if form.is_valid():
             filter_category = form.cleaned_data['category']
